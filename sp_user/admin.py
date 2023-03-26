2f6574cf478a5db3e55ce049f6c5324285c2d91d
박성식 <sosn3@naver.com>
2023-03-26 23:44:41 +0900

    app 생성

--- sp_user/admin.py
+++ sp_user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sp_user
 
 # Register your models here.
+class SpuserAdmin(admin.ModelAdmin):
+    # dupple
+    list_display = ('username','password')
+
+admin.site.register(Sp_user,SpuserAdmin)
--- sp_user/models.py
+++ sp_user/models.py
@@ -8,6 +8,12 @@ class Sp_user(models.Model):
     password = models.CharField(max_length=64,verbose_name="비밀번호")
     registered_date = models.DateTimeField(auto_now_add=True,verbose_name="등록일자")
     
+    # list의 명칭을 정의
+    def __str__(self):
+        return self.username
     
     class Meta:
         db_table = 'user_info' # 테이블명
+        # 앱의 명칭?
+        verbose_name = '부쳇_장고 사용자' 
+        verbose_name_plural = '부쳇_장고 사용자'
